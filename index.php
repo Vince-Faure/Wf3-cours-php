77b6b04cd517de562ed199d23711337834522b09
JackAdamsJenkins <clensx@gmail.com>
2023-06-15 10:57:01 +0200

    Début cours PHP

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bonjour</h1>
+    <?php 
+        echo "<p>Lorem ipsum</p>";
+    ?>
+
+    <h2>
+        <?php 
+            // Instructions
+            // Autant d'instructions que l'on souhaite
+            echo "Mon super titre 2 <br>";
+
+            // Double quote (guillemet)
+            echo "Suite du titre";
+
+            // Single quote '
+            echo 'Le contenu que je veux écrire';
+
+             // Double quote (guillemet)
+             echo ("Suite du titre");
+
+             // Single quote '
+             echo ('Le contenu que je veux écrire');
+
+            // Ecriture des variables
+            // le camel case > 
+                // Première lettre de la variable en minuscule
+                // Chaque première lettre de chaque mot en majuscule
+            $maVariable;
+            $maSuperVariableQuiEstTropLongue;
+
+
+
+        ?>
+    </h2>
+</body>
+</html>
