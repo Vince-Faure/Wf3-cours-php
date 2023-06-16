3ca666ca263cae28325893fb92182b74026f3fcf
vinceFaure <faurvincent@gmail.com>
2023-06-16 10:09:44 +0200

    while resolution

--- TP/tp03.php
+++ TP/tp03.php
@@ -1,11 +1,22 @@
 <?php
 
-/*Ecrire une boucle affichant tout les nomres impairs jusqu'a 100(inclus) */
+/*Ecrire une boucle affichant tout les nomres impairs jusqu'a 100(inclus)
 
 
-for( $i=1; $i <= 100; $i += 2){
+for( $i=1; $i <= 100; ){
 
-   echo "nombre pair = ".$i." <br />";
+  
+}
+ */
+
+
+$i = 0;
+while ($i <= 100){
+echo "nombre pair = ".$i." <br />";
+$i += 2;
+
+ 
 }
 
 ?>
+
